finalize link invest-operation with energy balance. bug in units

preprocess/functions/time_series_aggregation.py
@@ -12,6 +12,7 @@ import logging
 import tsam.timeseriesaggregation as tsam
 from model.objects.energy_system import EnergySystem
 from model.objects.element import Element
+from model.objects.carrier import Carrier
 from model.objects.technology.technology import Technology
 from model.objects.technology.storage_technology import StorageTechnology
 
@@ -222,10 +223,12 @@ class TimeSeriesAggregation():
         headerSetTimeSteps    = EnergySystem.getAnalysis()['headerDataInputs']["setTimeSteps"][0]
         oldOrderTimeSteps   = element.orderTimeSteps
         for timeSeries in element.rawTimeSeries:
-            oldTimeSeries   = getattr(element,timeSeries)
-            newIndex        = pd.MultiIndex.from_product(idx if idx.name != headerSetTimeSteps else setTimeStepsOperation for idx in oldTimeSeries.index.levels)
-            newTimeSeries   = pd.Series(index = newIndex)
-            a=1
+            oldTimeSeries   = getattr(element,timeSeries).unstack(headerSetTimeSteps)
+            newTimeSeries   = pd.DataFrame(index = oldTimeSeries.index, columns=setTimeStepsOperation)
+            idxOld2New      = [np.unique(oldOrderTimeSteps[np.argwhere(idx == orderTimeSteps)]) for idx in setTimeStepsOperation]
+            newTimeSeries   = newTimeSeries.apply(lambda row: oldTimeSeries[idxOld2New[row.name][0]],axis=0).stack()
+            # overwrite time series
+            setattr(element,timeSeries,newTimeSeries)
 
     @classmethod
     def setAggregationIndicators(cls, element, setEnergyBalanceIndicator=False):
@@ -275,9 +278,12 @@ class TimeSeriesAggregation():
         logging.info("\n--- Time series aggregation ---")
         TimeSeriesAggregation()
 
-        # calculate storage level time steps
         for element in Technology.getAllElements():
+            # calculate the time steps in operation to link with investment time steps
             cls.calculateTimeStepsLinkInvestOperation(element)
             if element in StorageTechnology.getAllElements():
                 # calculate time steps of storage levels
                 element.calculateTimeStepsStorageLevel()
+        # calculate new time steps of energy balance
+        for element in Carrier.getAllElements():
+            cls.calculateTimeStepsEnergyBalance(element)