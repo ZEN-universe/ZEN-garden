a9a66e2f9622b20c40763fe6c7d2deaa4d9ee96e
mapr7 <manonpr@live.fr>
2022-03-29 18:13:55 +0200

    Implemented the saving functionality, post-processing can also be run from the command line as long as the raw data was saved previously, still some additional things to add but basic concept is working

--- postprocess/results.py
+++ postprocess/results.py
@@ -28,26 +28,57 @@ class Postprocess:
     modelName = str()
 
 
-    def __init__(self, model, **kwargs):
+    def __init__(self, model=None, **kwargs):
         """postprocessing of the results of the optimization
         :param model:     optimization model
         :param pyoDict:   input data dictionary
         :param modelName: model name used for the directory to save the results in"""
 
-        self.model     = model.model
-        self.system    = model.system
-        self.analysis  = model.analysis
+        # self.model     = model.model
+        # self.system    = model.system
+        # self.analysis  = model.analysis
         self.modelName = kwargs.get('modelName', self.modelName)
         self.nameDir   = f'./outputs/results{self.modelName}/'
 
-        # if self.analysis['postprocess']:
-        self.makeDirs()
-        self.getVarValues()
-        #self.getParamValues()
-        self.saveResults()
-        self.plotResults()
-        # else:
-            # self.saveModel()
+        # self.makeDirs()
+        # self.getVarValues()
+        # self.saveResults()
+        # self.plotResults()
+
+        # case where the class is called from the command line
+        if model==None:
+            self.loadParam()
+            self.loadVar()
+            self.loadSystem()
+            self.loadAnalysis()
+            self.process()
+
+        # case where we are called from compile but should not perform the post-processing
+        elif model.analysis['postprocess']==False:
+
+            self.model     = model.model
+            self.system    = model.system
+            self.analysis  = model.analysis
+
+            self.makeDirs()
+            self.saveParam()
+            self.saveVar()
+            self.saveSystem()
+            self.saveAnalysis()
+
+        # case where we should run the post-process as normal
+        else:
+
+            self.model     = model.model
+            self.system    = model.system
+            self.analysis  = model.analysis
+
+            self.makeDirs()
+            self.saveParam()
+            self.saveVar()
+            self.saveSystem()
+            self.saveAnalysis()
+            self.process()
 
     def makeDirs(self):
         """create results directory"""
@@ -74,6 +105,27 @@ class Postprocess:
             # save params in a dataframe
             self.createDataframe(param, self.paramDict, self.paramDf)
 
+    def saveParam(self):
+        """ Saves the Param values to pickle files which can then be
+        post-processed immediately or loaded and postprocessed at some other time"""
+
+        # get all the param values from the model and store in a dict
+        for param in self.model.component_objects(pe.Param, active=True):
+            # sava params in a dict
+            self.paramDict[param.name] = dict()
+            for index in param:
+                self.paramDict[param.name][index] = pe.value(param[index])
+
+        # save the param dict to a pickle
+        with open(f'{self.nameDir}params/paramDict.pickle', 'wb') as file:
+            pickle.dump(self.paramDict, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def loadParam(self):
+        """ Loads the Param values from previously saved pickle files which can then be
+        post-processed """
+        with open(f'{self.nameDir}params/paramDict.pickle', 'rb') as file:
+            self.paramDict = pickle.load(file)
+
     def getVarValues(self):
         """get the values assigned to each variable"""
 
@@ -89,32 +141,99 @@ class Postprocess:
                 # save vars in a DataFrame
                 self.createDataframe(var, self.varDict, self.varDf)
 
-    def createDataframe(self, obj, dict, df):
+    def saveVar(self):
+        """ Saves the variable values to pickle files which can then be
+        post-processed immediately or loaded and postprocessed at some other time"""
+
+        # get all the variable values from the model and store in a dict
+        for var in self.model.component_objects(pe.Var, active=True):
+            if 'constraint' not in var.name and 'gdp' not in var.name:
+                # save vars in a dict
+                self.varDict[var.name] = dict()
+                for index in var:
+                    try:
+                        self.varDict[var.name][index] = var[index].value
+                    except:
+                        pass
+
+        # save the variable dict to a pickle
+        with open(f'{self.nameDir}vars/varDict.pickle', 'wb') as file:
+            pickle.dump(self.varDict, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def loadVar(self):
+        """ Loads the variable values from previously saved pickle files which can then be
+        post-processed """
+        with open(f'{self.nameDir}vars/varDict.pickle', 'rb') as file:
+            self.varDict = pickle.load(file)
+
+    def saveSystem(self):
+        with open(f'{self.nameDir}System.pickle', 'wb') as file:
+            pickle.dump(self.system, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def saveAnalysis(self):
+        with open(f'{self.nameDir}Analysis.pickle', 'wb') as file:
+            pickle.dump(self.analysis, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def loadSystem(self):
+        """ Loads the system object from previously saved pickle files which can then be
+        post-processed """
+        with open(f'{self.nameDir}System.pickle', 'rb') as file:
+            self.system = pickle.load(file)
+
+    def loadAnalysis(self):
+        """ Loads the analysis object from previously saved pickle files which can then be
+        post-processed """
+        with open(f'{self.nameDir}Analysis.pickle', 'rb') as file:
+            self.analysis = pickle.load(file)
+
+    # def createDataframe(self, obj, dict, df):
+    #     """ save data in dataframe"""
+    #     if dict[obj.name]:
+    #         if list(dict[obj.name].keys())[0] == None:
+    #             # [index, capacity]
+    #             df[obj.name] = pd.DataFrame(dict[obj.name].values(), columns=self.analysis['headerDataOutputs'][obj.name])
+    #             self.trimZeros(obj, self.varDf, df[obj.name].columns.values)
+    #             print(df)
+    #         elif type(list(dict[obj.name].keys())[0]) == int:
+    #             # seems like we never come in here
+    #             print("DID SOMETHING COME IN HERE??")
+    #             df[obj.name] = pd.DataFrame(dict[obj.name].values(), index=list(dict[obj.name].keys()), columns=self.analysis['headerDataOutputs'][obj.name])
+    #             self.trimZeros(obj, self.varDf, df[obj.name].columns.values)
+    #             print(df)
+    #         else:
+    #             # [tech, node, time, capacity]
+    #             df[obj.name] = pd.DataFrame(dict[obj.name].values(),index=pd.MultiIndex.from_tuples(dict[obj.name].keys())).reset_index()
+    #             df[obj.name].columns = self.analysis['headerDataOutputs'][obj.name]
+    #             self.trimZeros(obj, self.varDf, df[obj.name].columns.values)
+    #             print(df)
+    #     else:
+    #         print(f'{obj.name} not evaluated in results.py')
+    def createDataframe(self, varName, dict, df):
         """ save data in dataframe"""
-        if dict[obj.name]:
-            if list(dict[obj.name].keys())[0] == None:
+        if dict[varName]:
+            if list(dict[varName].keys())[0] == None:
                 # [index, capacity]
-                df[obj.name] = pd.DataFrame(dict[obj.name].values(), columns=self.analysis['headerDataOutputs'][obj.name])
-                self.trimZeros(obj, self.varDf, df[obj.name].columns.values)
+                df[varName] = pd.DataFrame(dict[varName].values(), columns=self.analysis['headerDataOutputs'][varName])
+                self.trimZeros(varName, self.varDf, df[varName].columns.values)
                 print(df)
-            elif type(list(dict[obj.name].keys())[0]) == int:
+            elif type(list(dict[varName].keys())[0]) == int:
                 # seems like we never come in here
                 print("DID SOMETHING COME IN HERE??")
-                df[obj.name] = pd.DataFrame(dict[obj.name].values(), index=list(dict[obj.name].keys()), columns=self.analysis['headerDataOutputs'][obj.name])
-                self.trimZeros(obj, self.varDf, df[obj.name].columns.values)
+                df[varName] = pd.DataFrame(dict[varName].values(), index=list(dict[varName].keys()), columns=self.analysis['headerDataOutputs'][varName])
+                self.trimZeros(varName, self.varDf, df[varName].columns.values)
                 print(df)
             else:
                 # [tech, node, time, capacity]
-                df[obj.name] = pd.DataFrame(dict[obj.name].values(),index=pd.MultiIndex.from_tuples(dict[obj.name].keys())).reset_index()
-                df[obj.name].columns = self.analysis['headerDataOutputs'][obj.name]
-                self.trimZeros(obj, self.varDf, df[obj.name].columns.values)
+                df[varName] = pd.DataFrame(dict[varName].values(),index=pd.MultiIndex.from_tuples(dict[varName].keys())).reset_index()
+                df[varName].columns = self.analysis['headerDataOutputs'][varName]
+                self.trimZeros(varName, self.varDf, df[varName].columns.values)
                 print(df)
         else:
             print(f'{obj.name} not evaluated in results.py')
 
-    def trimZeros(self, obj, df, c=[0]):
+    def trimZeros(self, varName, df, c=[0]):
         """ Trims out the zero rows in the dataframe """
-        df[obj.name] = df[obj.name].loc[~(df[obj.name][c[-1]]==0)]
+        df[varName] = df[varName].loc[~(df[varName][c[-1]]==0)]
 
         # TODO: handle the case where you are left with an empty dataframe
         # --> maybe put the check in saveResults() and either have no csv for
@@ -135,12 +254,12 @@ class Postprocess:
         for varName, df in self.varDf.items():
             df.to_csv(f'{self.nameDir}vars/{varName}.csv', index=False)
 
-    def saveModel(self):
-        # filename = f'{self.nameDir}'+self.system["modelName"]+'.pickle"'
-        # print(filename)
-        # with open(filename, 'wb') as file:
-        #     pickle.dump(self.analysis, file, protocol=pickle.HIGHEST_PROTOCOL)
-        return
+    def process(self):
+        print(self.varDict.items())
+        for var,dic in self.varDict.items():
+            self.createDataframe(var, self.varDict, self.varDf)
+            self.varDf[var].to_csv(f'{self.nameDir}vars/{var}.csv', index=False)
+            self.plotResults()
 
     def plotResults(self):
         for varName, df in self.varDf.items():
@@ -180,10 +299,9 @@ class Postprocess:
     # indexNames  = self.getProperties(getattr(self.model, varName).doc)
     # self.varDf[varName] = pd.DataFrame(varResults, index=pd.MultiIndex.from_tuples(indexValues, names=indexNames))
 
-    if __name__ == "__main__":
-        today = datetime.date()
-        filename = "model_" + today.strftime("%Y-%m-%d")
-        with open("./outputs/{filename}.pkl", 'rb') as inp:
-            tech_companies = pickle.load(inp)
-
-        evaluation = Postprocess(model, modelName='test')
+if __name__ == "__main__":
+    # today = datetime.date()
+    # filename = "model_" + today.strftime("%Y-%m-%d")
+    # with open("./outputs/{filename}.pkl", 'rb') as inp:
+    #     tech_companies = pickle.load(inp)
+    evaluation = Postprocess(modelName='Manon_test')
