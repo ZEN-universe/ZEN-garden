e4c5e8f297b964f06aa7ddb0a90b288d4301d475
aganter <aganter@ethz.ch>
2022-03-31 14:24:12 +0200

    update config.py

--- config.py
+++ config.py
@@ -12,11 +12,11 @@ import importlib.util
 ## Analysis - Defaul dictionary
 analysis = default_config.analysis
 ## Solver - Defaul dictionary
-
+solver = default_config.solver
 
 ## Analysis - settings update compared to default values
 analysis                                            = default_config.analysis         # get default settings from default config
-analysis["dataset"]                                 = "NUTS0_HSC"                     # select a dataset
+analysis["dataset"]                                 = "Output"                        # select a dataset
 analysis["objective"]                               = "TotalCost"                     # choose from "TotalCost", "TotalCarbonEmissions", "Risk"
 analysis["useExistingCapacities"]                   = False                           # use greendfield or brownfield approach
 
