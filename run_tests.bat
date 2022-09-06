94d59d57e327f743ec7925fe641b5ada72c28913
Janis Fluri <janis.fluri@hotmail.com>
2022-09-06 09:45:55 +0200

    propagation of error codes

--- run_tests.bat
+++ run_tests.bat
@@ -7,11 +7,16 @@ cmd /d /c "%~f0" :main %*
 Rem This will be executed even if main fails
 
 Rem This gets the directory name of the batch file, i.e. ZEN-garden
+set current=%~dp0
 for %%a in ("%~dp0\.") do set "parent=%%~nxa"
 Rem go to the dir
 cd %temp%\%parent%
 Rem Report coverage
 coverage report -m
+cd %current%
+
+Rem Error level probagation from before
+if !errorlevel! neq 0 exit /b 1
 exit /b
 
 :main
@@ -70,6 +75,7 @@ for /f "tokens=*" %%G in ('dir /b /s /a:d "data\test_*"') do (
 
     Rem run the coverage
     coverage run --source="./model,./preprocess,./postprocess" -a compile.py
+    if !errorlevel! neq 0 exit /b !errorlevel!
   )
 )
 exit /b
