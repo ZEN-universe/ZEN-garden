374e7a3399831d661d3186039efefa491cb926da
Janis Fluri <janis.fluri@hotmail.com>
2022-09-29 10:52:46 +0200

    Added main function to execute the package

---
+++ zen_garden/__main__.py
@@ -0,0 +1,51 @@
+"""===========================================================================================================================================================================
+Title:        ZEN-GARDEN
+Created:      September-2022
+Authors:      Janis Fluri (janis.fluri@id.ethz.ch)
+              Alissa Ganter (aganter@ethz.ch)
+              Davide Tonelli (davidetonelli@outlook.com)
+              Jacob Mannhardt (jmannhardt@ethz.ch)
+Organization: Laboratory of Reliability and Risk Engineering, ETH Zurich
+
+Description:  Compilation  of the optimization problem.
+==========================================================================================================================================================================="""
+from ._internal import compile
+import importlib.util
+import argparse
+import sys
+
+def main(args=None):
+    """
+    main function of ZEN-Garden
+    :param args: Arguments to parse
+    """
+    if args is None:
+        args = sys.argv[1:]
+
+    # parse the args
+    description = "Run ZEN-Garden with a given config file. Per default, the config file will be read out from the " \
+                  "current working directory. You can specify a config file with the --config argument. However, " \
+                  "not that the output directory will always be the current working directory, independent of the " \
+                  "dataset specified in the config file."
+    parser = argparse.ArgumentParser(description=description, add_help=True,
+                                     usage="usage: python -m zen_garden [-h] [--config CONFIG] [--dataset DATASET]")
+
+    parser.add_argument("--config", required=False, type=str, default="config.py",
+                        help="The config file used to run the pipeline, "
+                             "defaults to config.py in the current directory.")
+    parser.add_argument("--dataset", required=False, type=str, default=None,
+                        help="Path to the dataset used for the run. IMPORTANT: This will overwrite the "
+                             "config.analysis['dataset'] attribute of the config file!")
+    args = parser.parse_args(args)
+
+    ### import the config
+    spec = importlib.util.spec_from_file_location("module", args.config)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    config = module.config
+
+    ### run
+    compile(config=config, dataset_path=args.dataset)
+
+if __name__ == "__main__":
+    main()
---
+++ zen_garden/_internal.py
@@ -0,0 +1,111 @@
+"""===========================================================================================================================================================================
+Title:        ZEN-GARDEN
+Created:      October-2021
+Authors:      Alissa Ganter (aganter@ethz.ch)
+              Davide Tonelli (davidetonelli@outlook.com)
+              Jacob Mannhardt (jmannhardt@ethz.ch)
+Organization: Laboratory of Reliability and Risk Engineering, ETH Zurich
+
+Description:  Compilation  of the optimization problem.
+==========================================================================================================================================================================="""
+import os
+import sys
+import logging
+import importlib.util
+import pkg_resources
+from   .preprocess.prepare             import Prepare
+from   .model.optimization_setup       import OptimizationSetup
+from   .postprocess.results            import Postprocess
+
+
+def compile(config, dataset_path=None):
+    """
+    This function runs the compile.py script that was used in ZEN-Garden prior to the package build, it is executed
+    in the __main__.py script
+    :param config: A config instance used for the run
+    :param dataset_path: If not None, used to overwrite the config.analysis["dataset"]
+    """
+    # SETUP LOGGER
+    log_format = '%(asctime)s %(filename)s: %(message)s'
+    log_path = os.path.join('outputs', 'logs')
+    os.makedirs(log_path, exist_ok=True)
+    logging.basicConfig(filename=os.path.join(log_path, 'valueChain.log'), level=logging.INFO,
+                        format=log_format, datefmt='%Y-%m-%d %H:%M:%S')
+    logging.captureWarnings(True)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    logging.getLogger().addHandler(handler)
+
+    # print the version
+    version = pkg_resources.require("zen_garden")[0].version
+    logging.info(f"Running ZEN-Garden version: {version}")
+
+    # prevent double printing
+    logging.propagate = False
+
+    # overwrite the path if necessary
+    if dataset_path is not None:
+        logging.info(f"Overwriting dataset to: {dataset_path}")
+        config.analysis["dataset"] = dataset_path
+    # get the abs path to avoid working dir stuff
+    config.analysis["dataset"] = os.path.abspath(config.analysis['dataset'])
+
+    ### System - load system configurations
+    system_path = os.path.join(config.analysis['dataset'], "system.py")
+    if not os.path.exists(system_path):
+        raise FileNotFoundError(f"system.py not found in dataset: {config.analysis['dataset']}")
+    spec    = importlib.util.spec_from_file_location("module", system_path)
+    module  = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    system  = module.system
+    config.system.update(system)
+
+    ### overwrite default system and scenario dictionaries
+    if config.system["conductScenarioAnalysis"]:
+        scenarios_path = os.path.abspath(os.path.join(config.analysis['dataset'], "scenarios.py"))
+        if not os.path.exists(scenarios_path):
+            raise FileNotFoundError(f"scenarios.py not found in dataset: {config.analysis['dataset']}")
+        spec        = importlib.util.spec_from_file_location("module", scenarios_path)
+        module      = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        scenarios   = module.scenarios
+        config.scenarios.update(scenarios)
+
+    # create a dictionary with the paths to access the model inputs and check if input data exists
+    prepare = Prepare(config)
+    # check if all data inputs exist and remove non-existent
+    prepare.checkExistingInputData()
+
+    # FORMULATE THE OPTIMIZATION PROBLEM
+    # add the elements and read input data
+    optimizationSetup           = OptimizationSetup(config.analysis, prepare)
+    # get rolling horizon years
+    stepsOptimizationHorizon    = optimizationSetup.getOptimizationHorizon()
+
+    # update input data
+    for scenario, elements in config.scenarios.items():
+        optimizationSetup.restoreBaseConfiguration(scenario, elements)  # per default scenario="" is used as base configuration. Use setBaseConfiguration(scenario, elements) if you want to change that
+        optimizationSetup.overwriteParams(scenario, elements)
+        # iterate through horizon steps
+        for stepHorizon in stepsOptimizationHorizon:
+            if len(stepsOptimizationHorizon) == 1:
+                logging.info("\n--- Conduct optimization for perfect foresight --- \n")
+            else:
+                logging.info(f"\n--- Conduct optimization for rolling horizon step {stepHorizon} of {max(stepsOptimizationHorizon)}--- \n")
+            # overwrite time indices
+            optimizationSetup.overwriteTimeIndices(stepHorizon)
+            # create optimization problem
+            optimizationSetup.constructOptimizationProblem()
+            # SOLVE THE OPTIMIZATION PROBLEM
+            optimizationSetup.solve(config.solver)
+            # add newly builtCapacity of first year to existing capacity
+            optimizationSetup.addNewlyBuiltCapacity(stepHorizon)
+            # add cumulative carbon emissions to previous carbon emissions
+            optimizationSetup.addCarbonEmissionsCumulative(stepHorizon)
+            # EVALUATE RESULTS
+            modelName = os.path.basename(config.analysis["dataset"])
+            if len(stepsOptimizationHorizon) > 1:
+                modelName += f"_MF{stepHorizon}"
+            if config.system["conductScenarioAnalysis"]:
+                modelName += f"_{scenario}"
+            evaluation = Postprocess(optimizationSetup, modelName=modelName)
