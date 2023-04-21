a37144017b0283f5f69331c2a205a7e0c92f5100
Janis Fluri <jafluri@phys.ethz.ch>
2023-04-21 09:04:23 +0200

    updated euler env to 3.11, setup working for euler with base dir

--- euler/create_env_euler.sh
+++ euler/create_env_euler.sh
@@ -7,11 +7,11 @@
 # actiavte the right modules
 # gcc 8.2 stack
 module load gcc/8.2.0
-# python 3.9.9
-module load python/3.9.9
+# python 3.11.2
+module load python/3.11.2
 # backend for the solvers
 module load glpk
-module load gurobi/9.5.1
+module load gurobi/10.0.1
 
 # create the env
 python -m venv zen_garden_env
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="zen_garden",
@@ -8,6 +8,7 @@ setup(
     author_email='foobar@baz.com, spameggs@joe.org',
     python_requires='>=3.8, <4',
     keywords='key1, key2',
-    packages=find_packages(include=["zen_garden.*"]),
+    packages=['zen_garden'],
+    package_dir={'zen_garden': 'zen_garden/'},
     project_urls={'ZEN-Garden': 'https://github.com/RRE-ETH/ZEN-garden'},
 )
