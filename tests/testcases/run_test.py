35450d7a1955a9f9ae30ce62e275a8902de31b13
jacob-mannhardt <97548957+jacob-mannhardt@users.noreply.github.com>
2023-08-18 10:20:26 +0200

    change analysis scenario test to "objective"

--- tests/testcases/run_test.py
+++ tests/testcases/run_test.py
@@ -437,4 +437,4 @@ if __name__ == "__main__":
     from config import config
     config.solver["keep_files"] = False
     folder_path = os.path.dirname(__file__)
-    test_6a(config,folder_path)
+    test_6c(config,folder_path)
--- tests/testcases/test_6c/scenarios.py
+++ tests/testcases/test_6c/scenarios.py
@@ -12,4 +12,4 @@ scenarios["1"] = {"system": {'reference_year': 2023,
                              'conduct_time_series_aggregation': True,
                              'unaggregated_time_steps_per_year': 8760,
                              'aggregated_time_steps_per_year': 1},
-                  'analysis': {"discount_rate": 0.03}}
+                  'analysis': {"objective": "total_carbon_emissions"}}
