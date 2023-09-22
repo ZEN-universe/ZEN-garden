c4f6093673320136593417894af0b65b87cd1a0f
Lukas-Kunz <114185605+Lukas-Kunz@users.noreply.github.com>
2023-09-22 11:41:45 +0200

    fix position of check_existing_technology_data

--- zen_garden/model/optimization_setup.py
+++ zen_garden/model/optimization_setup.py
@@ -47,10 +47,10 @@ class OptimizationSetup(object):
         self.input_data_checks.optimization_setup = self
         # create a dictionary with the paths to access the model inputs and check if input data exists
         self.create_paths()
-        # check if all needed data inputs for the chosen technologies exist and remove non-existent
-        self.input_data_checks.check_existing_technology_data()
         # dict to update elements according to scenario
         self.scenario_dict = ScenarioDict(scenario_dict, self.system, self.analysis)
+        # check if all needed data inputs for the chosen technologies exist and remove non-existent
+        self.input_data_checks.check_existing_technology_data()
         # empty dict of elements (will be filled with class_name: instance_list)
         self.dict_elements = defaultdict(list)
         # pe.ConcreteModel
