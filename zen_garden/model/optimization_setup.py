faec8e45eef673fe793d6f1489af6eed75ff1d2b
jacob-mannhardt <97548957+jacob-mannhardt@users.noreply.github.com>
2023-12-19 15:57:36 +0100

    bug fix index_names

--- zen_garden/model/optimization_setup.py
+++ zen_garden/model/optimization_setup.py
@@ -628,7 +628,7 @@ class OptimizationSetup(object):
             if index_names is None:
                 raise ValueError(f"Index names for {component_name} not specified")
             custom_set, index_list = calling_class.create_custom_set(index_names, self)
-            component_data, attribute_is_series = self.get_attribute_of_all_elements(calling_class, component_name, capacity_types=capacity_types, return_attribute_is_series=True,index_names=index_names)
+            component_data, attribute_is_series = self.get_attribute_of_all_elements(calling_class, component_name, capacity_types=capacity_types, return_attribute_is_series=True)
             if np.size(custom_set):
                 if attribute_is_series:
                     component_data = pd.concat(component_data, keys=component_data.keys())
