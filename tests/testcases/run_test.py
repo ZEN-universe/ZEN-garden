ae36a98fae621ba4144e4ca53432f8f73e805cbe
jacob-mannhardt <97548957+jacob-mannhardt@users.noreply.github.com>
2024-06-27 10:30:05 +0200

    bug fixes test_2b in new xarray version

--- tests/testcases/run_test.py
+++ tests/testcases/run_test.py
@@ -596,4 +596,4 @@ if __name__ == "__main__":
 
     config.solver["keep_files"] = False
     folder_path = os.path.dirname(__file__)
-    test_1a(config, folder_path)
+    test_2b(config, folder_path)
