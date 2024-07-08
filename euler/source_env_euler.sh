f0b95e1366efdfa422b3a2613922a02feaf1b3b7
LSchmidtEn <150145178+LSchmidtEn@users.noreply.github.com>
2024-07-08 23:03:06 +0200

    Update Euler

--- euler/source_env_euler.sh
+++ euler/source_env_euler.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# swap to the right module /env2lmod is an alias for this
-# space between the dot is because it is an absolute path
-. /cluster/apps/local/env2lmod.sh
 
 # actiavte the right modules
-# gcc 8.2 stack
-module load gcc/8.2.0
-# python 3.11.2
-module load python/3.11.2
+# gcc 12.2 stack
+module load stack/2024-06
+module load gcc/12.2.0
+
+# python 3.11.6
+module load python/3.11.6
 
 # backend for the solvers
 module load glpk
-module load gurobi/10.0.1
+module load gurobi/10.0.3
 
 # activate the env from anywhere
-# $BASH_SOURCE contains the path that was sourced 
+# $BASH_SOURCE contains the path that was sourced
 source $(realpath $BASH_SOURCE | xargs dirname)/zen_garden_env/bin/activate
