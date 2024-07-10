2f45b774633c711555079d472b5a274efce1b009
johburger <63449992+johburger@users.noreply.github.com>
2024-07-10 11:10:18 +0200

    adjust for unit ton_stored

--- zen_garden/preprocess/unit_handling.py
+++ zen_garden/preprocess/unit_handling.py
@@ -563,7 +563,7 @@ class UnitHandling:
         :return: dict with attribute name and reduced unit
         """
         # dictionary which assigns unit dimensions to corresponding base unit namings
-        distinct_dims = {"money": "[currency]", "distance": "[length]", "time": "[time]", "emissions": "[mass]"}
+        distinct_dims = {"money": "[currency]", "distance": "[length]", "time": "[time]", "emissions": "[mass]", "emissions_stored": "[mass_stored]"}
         unit = unit_specs["unit_in_base_units"]
         unit_category = unit_specs["unit_category"]
         for dim, dim_name in distinct_dims.items():
