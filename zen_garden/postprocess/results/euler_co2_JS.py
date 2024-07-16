93eb2fc01326726a38d2df1f22ac5b70474dfc95
Jara Späte Personal Laptop <jara.spaete@gmail.com>
2024-07-16 09:32:41 -0700

    uncommented for euler

--- zen_garden/postprocess/results/euler_co2_JS.py
+++ zen_garden/postprocess/results/euler_co2_JS.py
@@ -105,46 +105,46 @@ def print_import_cost_carrier(res_basic, folder, output_path):
 
 
 def main():
-    folder = "county_1507/county_1507_1"
-    output_path = "../../../data/outputs/"
+    folder = "county_1507_3"
+    output_path = "../../../outputs/"
     directory = os.path.join(output_path, folder)
     res_basic = Results(directory)
 
     get_co2_emissions(res_basic, folder, output_path)
     print_import_cost_carrier(res_basic, folder, output_path)
 
-    # node = 'NE_BO015'
-    # scenarios = ['scenario_','scenario_0','scenario_25','scenario_75','scenario_100']
+    node = 'IN_BE007'
+    scenarios = ['scenario_','scenario_0','scenario_25','scenario_75','scenario_100']
 
-    # # plot_energy_balance(res_basic, node, scenarios, directory, save_fig=True)
+    plot_energy_balance(res_basic, node, scenarios, directory, save_fig=True)
 
-    # #################################################################################
-    # list_folders = ["county_1107_288_1", "county_1107_288_2", "county_1107_288_3", "county_1107_288_4", "county_1107_288_5", "county_1107_288_6", "county_1107_288_7"]
-    # area = 'United States'
-    # custom_order = ['','100','75','25','0']
-    # pareto_group = 'scenario_name'
-    # specific_scenario_name = 'analysis_110724'
-    #     # Define unit conversions
-    # units = {
-    #     'co2': 'tons',
-    #     'cost': '1000000*USD',
-    #     'water_energy': '1000*meter ** 3',
-    #     'energy': 'MWh',
-    #     'power': 'MW'
-    # }
-    # #results_JS.plot_pareto_front(folder, output_path, units, specific_scenario_name, custom_order, area, pareto_group, list_folders=list_folders, save_fig=True)
+    #################################################################################
+    list_folders = ["county_1507_3"]
+    area = 'United States'
+    custom_order = ['','100','75','25','0']
+    pareto_group = 'scenario_name'
+    specific_scenario_name = 'analysis_110724'
+    # Define unit conversions
+    units = {
+        'co2': 'tons',
+        'cost': 'USD',
+        'water_energy': '1000*meter ** 3',
+        'energy': 'MWh',
+        'power': 'MW'
+    }
+    results_JS.plot_pareto_front(folder, output_path, units, specific_scenario_name, custom_order, area, pareto_group, list_folders=list_folders, save_fig=True)
 
-    # #################################################################################
+    #################################################################################
 
-    # scenarios = ['scenario_0','scenario_25','scenario_75','scenario_100']
+    scenarios = ['scenario_0','scenario_25','scenario_75','scenario_100']
 
 
-    # df_caps_all_filtered = results_JS.aggregate_to_states(folder, output_path, 'capacity_addition', scenarios, list_folders)
-    # plot_results.plot_boxplot_capacities_states(scenarios, df_caps_all_filtered, output_path, folder)
-    # df_group, df_group_filtered = results_JS.import_flow_data(folder, output_path, scenarios, column_name='flow_import', filter_carriers= ['electricity', 'diesel'], list_folders= list_folders)
-    # plot_results.plot_boxplot_energy_states(scenarios, df_group_filtered, folder, output_path, target_column='flow_import')
-    # df_group_demand, df_group_filtered_demand = results_JS.import_flow_data(folder, output_path, scenarios, 'demand', filter_carriers=['irrigation_water'], list_folders= list_folders)
-    # plot_results.plot_boxplot_energy_states(scenarios, df_group_filtered_demand, folder, output_path, 'demand')
+    df_caps_all_filtered = results_JS.aggregate_to_states(folder, output_path, 'capacity_addition', scenarios, list_folders)
+    plot_results.plot_boxplot_capacities_states(scenarios, df_caps_all_filtered, output_path, folder)
+    df_group, df_group_filtered = results_JS.import_flow_data(folder, output_path, scenarios, column_name='flow_import', filter_carriers= ['electricity', 'diesel'], list_folders= list_folders)
+    plot_results.plot_boxplot_energy_states(scenarios, df_group_filtered, folder, output_path, target_column='flow_import')
+    df_group_demand, df_group_filtered_demand = results_JS.import_flow_data(folder, output_path, scenarios, 'demand', filter_carriers=['irrigation_water'], list_folders= list_folders)
+    plot_results.plot_boxplot_energy_states(scenarios, df_group_filtered_demand, folder, output_path, 'demand')
 
 if __name__ == "__main__":
     main()
