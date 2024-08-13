b4da7264e3072baf1db255b96bde0ab8e1b8ecc1
jacob-mannhardt <97548957+jacob-mannhardt@users.noreply.github.com>
2024-08-13 10:58:23 +0200

    - align yearly-operational time steps to be in more intuitive order, - clean up pyproject.toml

--- tests/testcases/run_test.py
+++ tests/testcases/run_test.py
@@ -597,4 +597,4 @@ if __name__ == "__main__":
 
     config.solver["keep_files"] = False
     folder_path = os.path.dirname(__file__)
-    test_5b(config, folder_path)
+    test_3c(config, folder_path)
