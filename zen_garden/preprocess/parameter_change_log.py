4b171b0e8e8a55f920a6a5fa1b5fea526fb46fb4
fradmm <fdemarco@ethz.ch>
2024-10-30 16:08:53 +0100

    Bug fix

---
+++ zen_garden/preprocess/parameter_change_log.py
@@ -0,0 +1,11 @@
+def parameter_change_log():
+    log_dict = {
+        "capacity_limit": "capacity_limit_outdated",
+
+        "flow_storage_inflow": {
+            "default_value": 0,
+            "unit": "capacity_addition_min"
+        }
+    }
+
+    return log_dict
