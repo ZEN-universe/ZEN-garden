642cc3692438118bdb27bf8d7226cd95ef84cd8f
nour-boulos <nboulos@ethz.ch>
2025-07-09 16:46:46 +0200

    Update parameter_change_log.py

--- zen_garden/preprocess/parameter_change_log.py
+++ zen_garden/preprocess/parameter_change_log.py
@@ -1,9 +1,5 @@
 def parameter_change_log():
     log_dict = {
-        "amortization_time": {
-            "default_value": 'inf',  # only 0, 1, or 'inf' are allowed
-            "unit": "lifetime"
-        },
       #  "updated_name": "outdated_name",
 
     #    "new_parameter_name": {
