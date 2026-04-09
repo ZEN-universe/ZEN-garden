alidrd/ZEN-garden-Ali

Implement simple plugin importer to replace PluginManager

tests/unit_tests/test_plugins_loader.py
@@ -1,0 +1,33 @@
+from zen_garden.plugins.loader import import_selection_of_plugins
+
+class TestPluginsLoader():
+
+
+    def test_import_selected_plugin_import_corresponding_module(self):
+        # Arrange
+        plugins = {"fake_plugin": {}}
+
+        # Act
+        result = import_selection_of_plugins(
+            plugins,
+            source_package="tests.unit_tests"
+        )
+        from tests.unit_tests.fake_plugin import plugin
+
+        # Assert
+        assert result["fake_plugin"] == plugin
+
+
+    def test_pass_config_to_selected_plugins(self):
+        # Arrange
+        plugins = {"fake_plugin": {"any_parameter": "any_value"}}
+
+        # Act
+        import_selection_of_plugins(
+            plugins,
+            source_package="tests.unit_tests"
+        )
+        from tests.unit_tests.fake_plugin import plugin
+
+        # Assert
+        assert plugin.config == plugins["fake_plugin"] 

zen_garden/optimization_setup.py
@@ -21,11 +21,11 @@ from zen_garden.model.component import Constraint, IndexSet, Parameter, Variable
 from zen_garden.model.element import Element
 from zen_garden.model.energy_system import EnergySystem
 from zen_garden.model.technology.technology import Technology
+from zen_garden.plugins.loader import import_selection_of_plugins
 from zen_garden.preprocess.parameter_change_log import parameter_change_log
 from zen_garden.preprocess.time_series_aggregation import TimeSeriesAggregation
 from zen_garden.preprocess.unit_handling import Scaling
 from zen_garden.utils import IISConstraintParser, ScenarioDict, StringUtils
-from zen_garden.plugin_manager import PluginManager, Hook
 
 
 class OptimizationSetup(object):
@@ -56,8 +56,7 @@ class OptimizationSetup(object):
                 verify the integrity of the input data.
 
         """
-        self.plugin_manager = PluginManager()
-        self.plugin_manager.register(config.plugins)
+        import_selection_of_plugins(config.plugins)
 
         self.analysis = copy.deepcopy(config.analysis)
         self.system = copy.deepcopy(config.system)
@@ -540,9 +539,10 @@ class OptimizationSetup(object):
 
     def construct_optimization_problem(self):
         """Constructs the optimization problem."""
-        Events.trigger(Event.before_optimization_construction)
-        self.plugin_manager.emit(Hook.BEFORE_OPTIMIZATION_CONSTRUCTION,
-                                 optimization_setup=self)
+        Events.trigger(
+            Event.before_optimization_construction,
+            optimization_setup=self
+        )
 
         # create empty ConcreteModel
         if self.solver.solver_dir is not None and not os.path.exists(
@@ -558,10 +558,10 @@ class OptimizationSetup(object):
         # define and construct components of self.model
         Element.construct_model_components(self)
 
-        Events.trigger(Event.after_optimization_construction)
-        self.plugin_manager.emit(Hook.AFTER_OPTIMIZATION_CONSTRUCTION,
-                                 optimization_setup=self,
-                                 model_instance=self.model)
+        Events.trigger(
+            Event.after_optimization_construction,
+            optimization_setup=self
+        )
 
         # Initiate scaling object
         self.scaling = Scaling(

ZEN-universe/ZEN-garden

tests/unit_tests/test_plugins_loader.py
@@ -1,0 +1,34 @@
+from zen_garden.plugins.loader import register_plugins
+
+
+class TestPluginsLoader():
+
+
+    def test_import_selected_plugin_import_corresponding_module(self):
+        # Arrange
+        plugins = {"fake_plugin": {}}
+
+        # Act
+        result = register_plugins(
+            plugins,
+            source_package="tests.unit_tests"
+        )
+        from tests.unit_tests.fake_plugin import plugin
+
+        # Assert
+        assert result["fake_plugin"] == plugin
+
+
+    def test_pass_config_to_selected_plugins(self):
+        # Arrange
+        plugins = {"fake_plugin": {"any_parameter": "any_value"}}
+
+        # Act
+        register_plugins(
+            plugins,
+            source_package="tests.unit_tests"
+        )
+        from tests.unit_tests.fake_plugin import plugin
+
+        # Assert
+        assert plugin.config == plugins["fake_plugin"] 

zen_garden/optimization_setup.py
@@ -21,6 +21,7 @@ from zen_garden.model.component import Constraint, IndexSet, Parameter, Variable
 from zen_garden.model.element import Element
 from zen_garden.model.energy_system import EnergySystem
 from zen_garden.model.technology.technology import Technology
+from zen_garden.plugins.loader import register_plugins
 from zen_garden.preprocess.parameter_change_log import parameter_change_log
 from zen_garden.preprocess.time_series_aggregation import TimeSeriesAggregation
 from zen_garden.preprocess.unit_handling import Scaling
@@ -55,6 +56,8 @@ class OptimizationSetup(object):
                 verify the integrity of the input data.
 
         """
+        register_plugins(config.plugins)
+
         self.analysis = copy.deepcopy(config.analysis)
         self.system = copy.deepcopy(config.system)
         self.solver = copy.deepcopy(config.solver)
@@ -536,7 +539,10 @@ class OptimizationSetup(object):
 
     def construct_optimization_problem(self):
         """Constructs the optimization problem."""
-        Events.trigger(Event.before_optimization_construction)
+        Events.trigger(
+            Event.before_optimization_construction,
+            optimization_setup=self
+        )
 
         # create empty ConcreteModel
         if self.solver.solver_dir is not None and not os.path.exists(
@@ -552,7 +558,10 @@ class OptimizationSetup(object):
         # define and construct components of self.model
         Element.construct_model_components(self)
 
-        Events.trigger(Event.after_optimization_construction)
+        Events.trigger(
+            Event.after_optimization_construction,
+            optimization_setup=self
+        )
 
         # Initiate scaling object
         self.scaling = Scaling(