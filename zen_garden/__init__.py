5945835f12a5a6c5c1480ad013622fc28f9d219d
Christoph Sven Funke <csfunke@yahoo.com>
2026-09-10 16:15:36 +0200

    Make generic types publicly accessible

--- zen_garden/__init__.py
+++ zen_garden/__init__.py
@@ -1,3 +1,7 @@
+from .model.component_types.constraint import GenericConstraint
+from .model.component_types.parameter import GenericParameter
+from .model.component_types.set import GenericSet
+from .model.component_types.variable import GenericVariable
 from .model.schema import ModelSchema
 from .plugin_system.events import Event, EventPublisher
 from .postprocess.comparisons import (
@@ -18,5 +22,9 @@ __all__ = [
     "compare_dicts",
     "Event",
     "EventPublisher",
+    "GenericConstraint",
+    "GenericParameter",
+    "GenericSet",
+    "GenericVariable",
     "ModelSchema",
 ]
